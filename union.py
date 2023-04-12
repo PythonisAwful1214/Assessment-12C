603274219d8caf1d33442dc5a744ebe98968af17
PythonisAwful1214 <nicolascallier@mines.edu>
2023-04-11 23:05:41 -0600

    union
    This is the union function

---
+++ union.py
@@ -0,0 +1,5 @@
+def union(lst1, lst2):
+    return list(set(lst1) | set(lst2))
+players2 = ["Melvin", "Martian", "Baka", "Xai", "Cody"]
+union(scores, players2)
+[5, 8, 10, 6, 4, "Melvin", "Martian", "Baka", "Xai", "Cody"]
